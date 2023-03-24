3d6e22753d0009243d7b08ba7d73dff37c4c3c09
wezragraham <wezragraham@gmail.com>
2023-03-24 11:53:30 -0600

    working on movement still

--- Assets/Scripts/KillerMovement.cs
+++ Assets/Scripts/KillerMovement.cs
@@ -15,10 +15,14 @@ public class KillerMovement : MonoBehaviour
     [SerializeField]
     float rotationSpeedMultiplier;
 
+    Animator myAnimator;
+    AudioSource mySound;
+
     // Start is called before the first frame update
     void Start()
     {
-
+        myAnimator = this.GetComponent<Animator>();
+        mySound = this.GetComponent<AudioSource>();
     }
 
     // Update is called once per frame
@@ -28,6 +32,7 @@ public class KillerMovement : MonoBehaviour
 
         if ((Vector3.Distance(player.transform.position, this.transform.position) < 3))
         {
+            myAnimator.SetBool("IsWalking", true);
             transform.LookAt(new Vector3(player.transform.position.x, this.transform.position.y, player.transform.position.z));
             transform.Translate(Vector3.forward * (movementSpeedMultiplier / 2) * Time.deltaTime);
         }
@@ -35,16 +40,24 @@ public class KillerMovement : MonoBehaviour
         {
             if (Vector3.Distance(new Vector3(playerStepLocation.x, this.transform.position.y, player.transform.position.z), this.transform.position) > 2)
             {
+                myAnimator.SetBool("IsWalking", true);
                 transform.LookAt(new Vector3(playerStepLocation.x, this.transform.position.y, player.transform.position.z));
                 transform.Translate(Vector3.forward * movementSpeedMultiplier * Time.deltaTime);
             }
             else
             {
-                transform.Rotate(Vector3.up * rotationSpeedMultiplier * Time.deltaTime);
+                myAnimator.SetBool("IsWalking", false);
             }
 
         }
 
-
+        if (myAnimator.GetBool("IsWalking") == true && mySound.isPlaying == false)
+        {
+            mySound.Play();
+        }
+        else if (myAnimator.GetBool("IsWalking") == false && mySound.isPlaying == true)
+        {
+            mySound.Stop();
+        }
     }
 }
